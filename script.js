97da17cafaa4a68b80cd8f439eb5c3b60e4ed6f5
Claudia Silva <mkal72@hotmail.com>
2025-12-10 19:26:47 -0300

    Adicionando interatividade na galeria

---
+++ script.js
@@ -0,0 +1,10 @@
+// Passo 1: Pegar todos os pets da página 
+
+const pets = document.querySelectorAll(".pet")
+
+// Passo 2: Pegamos os elementos do card principal
+
+// Estes são os campos (lugares) onde vamos colocar as informações do pet clicado
+const nome = document.getElementById("nomeDoPet")
+const descricao = document.getElementById("descricao")
+const fotoGrande = document.getElementById("fotoGrande")
